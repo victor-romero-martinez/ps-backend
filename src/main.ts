512cf1cfa7979e748a13bd8b5fad0ff13e168821
victor-romero-martinez <victordav147@gmail.com>
2024-05-09 09:56:34 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT ?? 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
